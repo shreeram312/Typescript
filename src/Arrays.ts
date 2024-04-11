57e5ea871d6ac8e77a18ba738453586d1251aca0
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-04-11 16:47:06 +0530

    today

--- src/Arrays.ts
+++ src/Arrays.ts
@@ -3,3 +3,16 @@ const superHeros: string[] = [];
 superHeros.push("Captain America", "Ironman");
 superHeros.unshift("Thor");
 console.log(superHeros);
+
+type User = {
+  name: string;
+  isAlive: boolean;
+};
+
+const heroPower: User[] = [];
+
+heroPower.push({ name: "wanda", isAlive: true });
+
+console.log(heroPower);
+
+export {};
