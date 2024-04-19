e968ca65f45a9ffe07fa3b44779829fa05ef4170
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-04-19 16:14:26 +0530

    Ts

---
+++ src/Tuple.ts
@@ -0,0 +1,11 @@
+let tUser: [string, number, boolean, number];
+
+tUser = ["d", 1, false, 2];
+
+console.log(tUser);
+
+let rgb: readonly [number, number, string];
+rgb = [255, 255, "sdjn"];
+console.log(rgb); // Output: [255, 255, "sdjn"]
+
+export {};
---
+++ src/Union.ts
@@ -0,0 +1,5 @@
+let ans: (string | number)[] = [1, 2, 3, "0"];
+
+console.log(ans);
+
+export {};
