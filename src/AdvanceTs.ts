a79ce8d9a5765d46c12c881cb2ae406a7dd678ba
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-07-16 19:53:33 +0530

    Partial

--- src/AdvanceTs.ts
+++ src/AdvanceTs.ts
@@ -1,7 +1,10 @@
-interface User {
+type User = {
+  id: string;
   name: string;
   age: number;
-}
+  email: string;
+  password: string;
+};
 
 // function SumofAge(user1: User, user2: User) {
 //   return user1.age + user2.age;
@@ -18,4 +21,25 @@ interface User {
 //   }
 // );
 
+type PropUpdate = Pick<User, "name" | "email" | "age">;
+
+type PartialPropUpdate = Partial<PropUpdate>;
+function updateUser(updatedProps: PartialPropUpdate) {
+  //hit the database
+
+  console.log(updatedProps.name);
+}
+
+updateUser({
+  name: "rammm",
+});
+
+const obj = {
+  name: "shreeteja",
+  age: 18,
+};
+
+obj.name = "teja";
+
+console.log(obj);
 export {};
