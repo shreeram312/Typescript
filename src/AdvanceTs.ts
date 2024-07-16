7e157d0aa5a37deee7dd3da7420e7ec5306bb7f5
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2024-07-16 18:42:55 +0530

    commit

---
+++ src/AdvanceTs.ts
@@ -0,0 +1,21 @@
+interface User {
+  name: string;
+  age: number;
+}
+
+// function SumofAge(user1: User, user2: User) {
+//   return user1.age + user2.age;
+// }
+
+// SumofAge(
+//   {
+//     name: "shree",
+//     age: 20,
+//   },
+//   {
+//     name: "ram",
+//     age: 22,
+//   }
+// );
+
+export {};
